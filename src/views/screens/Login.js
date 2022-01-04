81af32b66025f6e005ca12aff1c59f1aa4a99ca2
Badar Masood <badarmasood99@gmail.com>
2022-01-04 19:54:58 +0500

    Edairy

--- src/views/screens/Login.js
+++ src/views/screens/Login.js
@@ -2,9 +2,9 @@
 // https://aboutreact.com/react-native-card-view/
 
 // import React in our code
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import all the components we are going to use
 import {
   SafeAreaView,
@@ -15,69 +15,60 @@ import {
   Image,
   TouchableOpacity,
   Header,
-  Button
-} from 'react-native';
+} from "react-native";
 
 //import Card
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
+import COLORS from "../../consts/colors";
 
 const LoginApp = ({ navigation }) => {
-    return (
-
-      
-
-     <SafeAreaView style={styles.container}>
-      <Card
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
         style={{
-          borderRadius: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+          borderRadius: 30,
+          justifyContent: "center",
+          alignItems: "center",
+          marginHorizontal:10,
+          backgroundColor: COLORS.primary,
+        }}
+      >
         <Image
-          source={require('../../assets/milk.png')}
+          source={require("../../assets/milk.png")}
           style={{ width: 170, height: 170 }}
         />
-      </Card>
-      <Card style={{ borderRadius: 20, marginTop: 20 , padding:20}}>
-        <Text style={styles.paragraph}> Login</Text>
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter the Email"
-          placeholderTextColor="black"
-        />
-        
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter Password"
-          placeholderTextColor="black"
-        />
+      </View>
 
+      <View style={{ borderRadius: 20, marginTop: 20, padding: 20 }}>
+        <Text style={styles.paragraph}> E DAIRY</Text>
 
-      </Card>
+        <TextInput style={styles.input} placeholder="Enter Email" />
+        <TextInput style={styles.input} placeholder="Enter Password" />
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Home')}>
-        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 18 }}>
-          {' '}
-          LOG IN{' '}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=>navigation.navigate('SignUp')}>
-        <Text
-          style={{
-            color: 'white',
-            fontWeight: 'bold',
-            fontSize: 16,
-            textAlign: 'center',
-          }}>
-          Make an Account {' '}
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <View
+            style={{
+              ...styles.btnContainer,
+              backgroundColor: COLORS.primary,
+            }}
+          >
+            <Text style={{ ...styles.title, color: COLORS.white }}>
+              L O G I N
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.row}>
+          <Text>Dont have an account? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+            <Text style={styles.link}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
-    
- 
-    )  
+  );
 };
 
 export default LoginApp;
@@ -85,42 +76,42 @@ export default LoginApp;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: '#007500',
+    padding: 5,
+    backgroundColor: "#fff",
   },
 
-  tex: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'black',
-    padding: 5,
-  },
-  texInput: {
-    borderWidth: 1,
-    borderRadius: 25,
-    color: 'black',
-    padding: 5,
-  },
-  texInput2: {
+  input: {
+    height: 60,
+    marginVertical: 12,
     borderWidth: 2,
-    borderRadius: 25,
-    marginTop: 20,
-    marginBottom: 20,
-    color: 'black',
+    borderRadius: 10,
     padding: 10,
+    borderColor: COLORS.primary,
   },
+
   paragraph: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 20,
-    color: 'black',
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "black",
   },
-  button: {
-    marginTop: 30,
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
+  row: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  link: {
+    fontWeight: "bold",
+    color: COLORS.primary,
+  },
+  title: { color: COLORS.white, fontWeight: "bold", fontSize: 18 },
+
+  btnContainer: {
+    backgroundColor: COLORS.primary,
+    height: 60,
+    marginTop:12,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- src/views/screens/SignUp.js
+++ src/views/screens/SignUp.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {
   SafeAreaView,
   Text,
@@ -9,69 +9,56 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
 //import Card
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
+import COLORS from "../../consts/colors";
 
 const Signup = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <Card
         style={{
-          borderRadius: 100,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+          borderRadius: 30,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: COLORS.primary,
+        }}
+      >
         <Image
-          source={require('../../assets/milk.png')}
+          source={require("../../assets/milk.png")}
           style={{ width: 170, height: 170 }}
         />
       </Card>
-      <Card style={{ borderRadius: 20, marginTop: 30 }}>
-        <Text style={styles.paragraph}> Register Now</Text>
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter Email"
-          placeholderTextColor="black"
-        />
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter Name"
-          placeholderTextColor="black"
-        />
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter Password"
-          placeholderTextColor="black"
-        />
-        
-        <TextInput
-          style={styles.texInput2}
-          placeholder="Enter Gender"
-          placeholderTextColor="black"
-        />
-      </Card>
-      <TouchableOpacity
-        style={styles.button}
-        // onPress={() => navigation.navigate('LoginScreen')}
-        >
-        <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 18 }}>
-          Sign Up{' '}
-        </Text>
-      </TouchableOpacity>
 
-      <TouchableOpacity onPress={()=>navigation.navigate('Login')} >
-        <Text
-          style={{
-            color: 'white',
-            fontWeight: 'bold',
-            fontSize: 16,
-            textAlign: 'center',
-          }}>
-          Login {' '}
-        </Text>
-      </TouchableOpacity>
+      <View style={{ borderRadius: 20, marginTop: 20, padding: 20 }}>
+        <Text style={styles.paragraph}> Sign Up</Text>
+
+        <TextInput style={styles.input} placeholder="Name" />
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Password" />
+
+        <TouchableOpacity activeOpacity={0.8}>
+          <View
+            style={{
+              ...styles.btnContainer,
+              backgroundColor: COLORS.primary,
+            }}
+          >
+            <Text style={{ ...styles.title, color: COLORS.white }}>
+              SIGN UP
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <View style={styles.row}>
+          <Text>Already have an account? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={styles.link}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -79,43 +66,44 @@ const Signup = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: '#007500',
+    padding: 5,
+    backgroundColor: "#fff",
   },
 
-  tex: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'black',
-    padding: 5,
-  },
-  texInput: {
-    borderWidth: 1,
-    borderRadius: 25,
-    color: 'black',
-    margin: 10,
-  },
-  texInput2: {
-    borderWidth: 1,
-    borderRadius: 25,
-    color: 'black',
-    margin: 10,
-    padding:10,
-  },
-  paragraph: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 8,
-    color: 'black',
-    fontStyle: 'italic',
-  },
-  button: {
-    marginTop: 30,
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
+  input: {
+    height: 60,
+    marginVertical: 12,
+    borderWidth: 2,
+    borderRadius: 10,
     padding: 10,
-    marginBottom: 10,
+    borderColor: COLORS.primary,
+  },
+
+  paragraph: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "black",
+  },
+  row: {
+    flexDirection: "row",
+    marginTop: 10,
+    justifyContent: "center",
+  },
+  link: {
+    fontWeight: "bold",
+    color: COLORS.primary,
+  },
+  title: { color: COLORS.white, fontWeight: "bold", fontSize: 18 },
+
+  btnContainer: {
+    backgroundColor: COLORS.primary,
+    height: 60,
+    borderRadius: 10,
+    marginTop:12,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
